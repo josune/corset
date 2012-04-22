3cd3a6582a40c0f60dc54677754f7da03547505f
josune <jdesosa@ikerlan.es>
2012-04-22 20:42:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-#include <stdio.h> 
- 
-int main(void){  
- 
-  int liczba;  
- 
-  for ( ; (scanf("%d",&liczba) > 0) && (liczba != 42) ; printf("%d\n", liczba) );  
- 
-  return 0;
+#include <stdio.h>
+int main(void){ int liczba; for ( ; (scanf("%d",&liczba) > 0) && (liczba != 42) ; printf("%d\n", liczba) ); return 0;
 }
